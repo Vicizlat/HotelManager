4ebd2d7f70cc99779f6fc64fe60ca7d5d611334d
Victor Zlatarev <vicizlat@gmail.com>
2020-06-13 20:12:16 +0300

    Simplified file updates

--- HotelManager/Program.cs
+++ HotelManager/Program.cs
@@ -1,58 +1,79 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Xml;
 
 namespace HotelManager
 {
     class Program
     {
-        private static string webAddress;
-        private const string ConfigFileName = "Config.xml";
-        private const string MainExeFileName = "HotelManagerGui.exe";
-        private const string CoreDllFileName = "Core.dll";
-        private const string CommandsDllFileName = "Commands.dll";
-        private const string HandlersDllFileName = "Handlers.dll";
-        private const string TemplatesDllFileName = "Templates.dll";
-        private const string DataFileName = "Reservations";
+        public static string LocalPath => Directory.CreateDirectory(Path.Combine(AppDataPath, "HotelManager")).FullName;
+        private static readonly string AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
         private static void Main()
         {
-            if (!TryUpdateFile(MainExeFileName)) return;
-            if (!TryUpdateFile(CoreDllFileName)) return;
-            if (!TryUpdateFile(CommandsDllFileName)) return;
-            if (!TryUpdateFile(HandlersDllFileName)) return;
-            if (!TryUpdateFile(TemplatesDllFileName)) return;
-            if (!TryUpdateFile(DataFileName)) return;
-            Process.Start(Path.Combine(FileHandler.LocalPath, MainExeFileName));
+            if (!File.Exists(Path.Combine(LocalPath, "Config.xml")))
+            {
+                Console.WriteLine("Напиши адрес за синхронизиране (напр. \"google.com\")");
+                if (!TryGetFile(Console.ReadLine(), "Config.xml")) return;
+            }
+            string[] fileNames = { "HotelManagerGui.exe", "Core.dll", "Commands.dll", "Handlers.dll", "Templates.dll", "Reservations" };
+            foreach (string fileName in fileNames)
+            {
+                if (!TryUpdateFile(fileName)) return;
+            }
+            Process.Start(Path.Combine(LocalPath, fileNames[0]));
         }
 
         public static bool TryUpdateFile(string fileName)
         {
-            if (!FileHandler.CheckFileExists(fileName) || !FileHandler.IsLocalFileNewer(fileName))
-            {
-                if (!FileHandler.TryGetFile(GetWebsiteAddress(), fileName))
-                {
-                    Console.WriteLine($"Не можах да сваля \"{fileName}\"");
-                    Console.ReadLine();
-                    return false;
-                }
-            }
-            return true;
+            if (File.Exists(Path.Combine(LocalPath, fileName)) && IsLocalFileNewer(fileName)) return true;
+            if (TryGetFile(GetXmlData("WebAddress"), fileName)) return true;
+            Console.WriteLine($"Не можах да сваля \"{fileName}\"");
+            return false;
+
         }
 
-        internal static string GetWebsiteAddress()
+        public static bool IsLocalFileNewer(string fileName)
         {
-            if (string.IsNullOrEmpty(webAddress))
+            try
             {
-                if (!FileHandler.CheckFileExists(ConfigFileName))
-                {
-                    Console.WriteLine("Напиши адрес за синхронизиране (напр. \"google.com\")");
-                    webAddress = $"https://{Console.ReadLine()}/HotelManager/";
-                    FileHandler.TryGetFile(webAddress, ConfigFileName);
-                }
-                else webAddress = $"https://{FileHandler.GetXmlData("WebAddress")}/HotelManager/";
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Path.Combine(GetXmlData("FtpAddress"), fileName));
+                request.Credentials = new NetworkCredential(GetXmlData("FtpUserName"), GetXmlData("FtpPassword"));
+                request.Method = WebRequestMethods.Ftp.GetDateTimestamp;
+                using FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                return File.GetLastWriteTime(Path.Combine(LocalPath, fileName)) >= response.LastModified;
             }
-            return webAddress;
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool TryGetFile(string webAddress, string fileName)
+        {
+            try
+            {
+                new WebClient().DownloadFile($"https://{webAddress}/HotelManager/{fileName}", Path.Combine(LocalPath, fileName));
+                Console.WriteLine($"Successfully downloaded \"{fileName}\".");
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine($"Failed to download\"{fileName}\".");
+                return false;
+            }
+        }
+
+        public static string GetXmlData(string element)
+        {
+            using XmlReader reader = XmlReader.Create(Path.Combine(LocalPath, "Config.xml"));
+            while (reader.Read())
+            {
+                if (reader.IsStartElement() && reader.Name == element) return reader.ReadElementContentAsString();
+            }
+            return element;
         }
     }
 }
