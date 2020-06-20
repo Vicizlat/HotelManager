4b341de54d6bc12f6dad1a71291edf4c32202e66
Victor Zlatarev <vicizlat@gmail.com>
2020-06-20 13:07:36 +0300

    Added dotNetDownloader

---
+++ DotNetInstaller/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace DotNetInstaller
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                string dotNetDownload = "https://download.visualstudio.microsoft.com/download/pr/";
+                if (args[0] == "x86") dotNetDownload += "df7b90d9-b93e-4974-85ef-c1de418bc186/e380e58bbd8505ebaee6c3abb23baade/";
+                if (args[0] == "x64") dotNetDownload += "86835fe4-93b5-4f4e-a7ad-c0b0532e407b/f4f2b1239f1203a05b9952028d54fc13/";
+                string dotNetFileName = $"windowsdesktop-runtime-3.1.5-win-{args[0]}.exe";
+                string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                string localPath = Directory.CreateDirectory(Path.Combine(appDataPath, "HotelManager")).FullName;
+                if (!File.Exists(Path.Combine(localPath, dotNetFileName)))
+                {
+                    Console.WriteLine($"Downloading {dotNetFileName}...");
+                    new WebClient().DownloadFile(Path.Combine(dotNetDownload, dotNetFileName), Path.Combine(localPath, dotNetFileName));
+                }
+                Console.WriteLine($"Installing {dotNetFileName}...");
+                Process.Start(Path.Combine(localPath, dotNetFileName), "-q")?.WaitForExit();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ DotNetInstaller/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotNetInstaller")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetInstaller")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee1cbf09-edb5-4018-80d8-2f9df16b2ee5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
